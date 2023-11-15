7a92a67b633b16c754d42d44693a1d48787a8b71
yunie <wldbs8241@naver.com>
2023-11-16 02:02:05 +0900

    Style: GPT 스타일 수정
    Former-commit-id: b232492c94089dd4a57f3e727693af6ff2b173a3

--- frontend/develog/src/components/Resume/SideRegion/GptRegion.style.tsx
+++ frontend/develog/src/components/Resume/SideRegion/GptRegion.style.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:974b8214717139aebdc35c81553c70093bef7d0609734f048bd3dd6ce8886ed8
+oid sha256:c07780cec2ae32ebe60ff7105c8df62dc25225d147bd5cac3a9fef0b6864ce53
 size 1344
